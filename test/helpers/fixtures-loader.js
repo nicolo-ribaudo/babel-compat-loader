561a77c588b61c9be6005e42e74cfab6ac1002ed
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-06-27 11:43:58 +0200

    Allow non-transform fixtures

--- test/helpers/fixtures-loader.js
+++ test/helpers/fixtures-loader.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const cp = require("child_process");
+const assert = require("assert");
 
 function loadOptions(fixturePath) {
   const optionsPath = path.join(fixturePath, "options.json");
@@ -17,6 +18,22 @@ module.exports = function loadFixtures(basePath) {
     const fixturePath = path.join(basePath, name);
     const { transform, versions, type } = loadOptions(fixturePath);
     const fileNames = fs.readdirSync(path.join(fixturePath, "src"));
+
+    if (!transform) {
+      assert(!versions);
+      assert.strictEqual(type, "snapshot");
+
+      fixtures.push({
+        name,
+        type,
+        fileNames,
+        path: fixturePath
+      });
+
+      continue;
+    }
+
+    assert(versions);
 
     for (const version of versions) {
       fixtures.push({
--- test/helpers/fixtures-runner.js
+++ test/helpers/fixtures-runner.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const assert = require("assert");
+
 const {
   mapAsync,
   regG,
@@ -50,22 +52,36 @@ async function execFixture(pathname, loaderPath, useLoader) {
 }
 
 module.exports = async function runFixture(fixture, loaderPath) {
-  const compiledFolder = path.join(fixture.path, fixture.version);
   const srcFolder = path.join(fixture.path, "src");
 
-  test(`${fixture.name} - babel ${fixture.version}`, async () => {
-    await rimraf(compiledFolder);
-    await compileFiles(fixture);
+  if (fixture.transform) {
+    assert(fixture.version);
 
-    const actual = await execFixture(compiledFolder, loaderPath, true);
+    const compiledFolder = path.join(fixture.path, fixture.version);
 
-    if (fixture.type === "snapshot") {
+    test(`${fixture.name} - babel ${fixture.version}`, async () => {
+      await rimraf(compiledFolder);
+      await compileFiles(fixture);
+
+      const actual = await execFixture(compiledFolder, loaderPath, true);
+
+      if (fixture.type === "snapshot") {
+        expect(actual.stdout).toMatchSnapshot("stdout");
+        expect(actual.stderr).toMatchSnapshot("stderr");
+      } else {
+        const expected = await execFixture(srcFolder, loaderPath);
+        expect(actual.stderr).toBe(expected.stderr);
+        expect(actual.stdout).toBe(expected.stdout);
+      }
+    });
+  } else {
+    assert.strictEqual(fixture.type, "snapshot");
+
+    test(fixture.name, async () => {
+      const actual = await execFixture(srcFolder, loaderPath, true);
+
       expect(actual.stdout).toMatchSnapshot("stdout");
       expect(actual.stderr).toMatchSnapshot("stderr");
-    } else {
-      const expected = await execFixture(srcFolder, loaderPath);
-      expect(actual.stderr).toBe(expected.stderr);
-      expect(actual.stdout).toBe(expected.stdout);
-    }
-  });
+    });
+  }
 };
